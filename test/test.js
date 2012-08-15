aa52f69e5ef035d1d998af36502f81e53d0196e6
Joshua Gross <joshua.gross@gmail.com>
2012-08-15 17:45:02 -0500

    Fixed unit test and removed some logging code.

--- test/test.js
+++ test/test.js
@@ -84,11 +84,11 @@ describe('JailKeeper', function () {
     });
   });
   it('should prevent writing to parent directory', function (done) {
+    this.timeout(0);
     fs.stat('test/parentDirWrite.txt', function (deetz) {
       if (deetz) {
         fs.unlinkSync('test/parentDirWrite.txt');
       }
-      this.timeout(0);
       var jail = new JailKeeper();
       var childProcess = jail.spawn('./writeToParentDirectory', [], { cwd: './test/fixtures' });
       var jailbroken = false;
